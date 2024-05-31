8ff9cfcf800c52d50beebbff903df0ca5d7ace87
Pomelo4455 <sm67588@gmail.com>
2024-05-31 02:41:55 -0300

    Styling improvements

--- src/app/page.js
+++ src/app/page.js
@@ -250,11 +250,17 @@ export default function Home() {
     <div className="min-h-screen bg-[#191B23] text-white overflow-x-hidden">
       <div className="bg-[#121418] flex flex-col md:flex-row md:px-20 w-full h-fit p-3 justify-around items-center">
         <div className="bg-[#121418] flex flex-col md:flex-row w-full h-fit p-4 items-center">
-          <img
-            src="https://www.mimic.fi/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo-navbar.a5d9ffd0.svg&w=384&q=75"
-            alt="Mimic Logo"
-            className="h-12 mb-4 md:mb-0"
-          />
+          <a
+            href="https://www.mimic.fi/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="https://www.mimic.fi/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo-navbar.a5d9ffd0.svg&w=384&q=75"
+              alt="Mimic Logo"
+              className="h-12 mb-4 md:mb-0"
+            />
+          </a>
           <h1 className="text-4xl font-semibold text-center md:flex-1 tracking-wide">
             Workflow Visualizer
           </h1>
@@ -263,7 +269,7 @@ export default function Home() {
             value={tokenAddress}
             onChange={handleTokenAddressChange}
             placeholder="Enter a token address"
-            className="rounded-lg bg-[#191B23] p-2 text-white border border-[#191B23] focus:outline-none focus:ring-1 focus:ring-[#6F5CE6] mt-4 md:mt-0"
+            className="rounded-lg bg-[#191B23] p-2 px-4 text-white border border-[#191B23] focus:outline-none focus:ring-1 focus:ring-[#6F5CE6] mt-4 md:mt-0"
           />
         </div>
       </div>
@@ -354,7 +360,7 @@ export default function Home() {
           overflow-y: hidden;
         }
         .highlight {
-          animation: highlight 2s;
+          animation: highlight 2.3s;
         }
         @keyframes highlight {
           0% {
